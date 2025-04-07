479e061eb1284fa089ecd7a1b04cec7a9327eded
王哥流浪天涯 <wxj995@gmail.com>
2025-04-07 11:24:32 -0700

    Update main.cpp

--- CppBasicExample/src/main.cpp
+++ CppBasicExample/src/main.cpp
@@ -30,6 +30,7 @@ void PrintStudent(Student &s){ //pass struct by reference
 }
 
 int main(){
+	cout<<"Practise 1: 4 methods to create struct objects"<< endl;
 	//Method #1 to create struct
 	Student s1;
 	s1.name = "Alex";
@@ -51,16 +52,21 @@ int main(){
 		{"Fiona, 32, 85"}
 	}
 
-	//Use pointer to visit struct
-	Student *p = &s1;
-
 	cout<<"Print student result on the screen!"<< endl;
 	cout<<"s1 Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
 	cout<<"s2 Name:"<<s2.name<<", Age="<<s2.age<<", Score="<<s2.score<<endl;
 	cout<<"s3 Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl;
 	for(int i = 0; i < 3; i++) cout<<"studentArray["<<i<<"] Name:"<<studentArray[i].name<<", Age="<<studentArray[i].age<<", Score="<<studentArray[i].score<<endl;
+
+
+	
+	cout<<"Practise 2: Use pointer to visit struct object"<< endl;
+	Student *p = &s1;
 	cout<<"p Name:"<<p->name<<", Age="<<p->age<<", Score="<<p->score<<endl;
 
+
+	
+	cout<<"Practise 3: A struct(teacher) contains another struct(student)"<< endl;
 	teacher t;
 	t.id = 10000;
 	t.name = "Garrett";
@@ -73,6 +79,9 @@ int main(){
 	cout<<"t Name:"<<t.name<<", Age="<<t.age<<", Score="<<t.score<<endl;
 	cout<<"t.stu Name:"<<t.stu.name<<", Age="<<t.stu.age<<", Score="<<t.stu.score<<endl;
 
+
+	
+	cout<<"Practise 4: Pass struct object as parameter"<< endl;
 	//pass struct by value
 	PrintStudent(s1);
 	cout<<"s1(in main) Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
@@ -84,6 +93,13 @@ int main(){
 	//pass struct by reference
 	PrintStudent(s3);
 	cout<<"s3(in main) Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl;
+
+
+	
+	cout<<"Practise 5: Use const in struct"<< endl;
+
+
+
 	
 	cout<<endl;
 	return 0;
