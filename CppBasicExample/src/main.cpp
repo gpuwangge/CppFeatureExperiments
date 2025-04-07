48cf18e49bc7f5215d578b21d0ab78ec873e5091
王哥流浪天涯 <wxj995@gmail.com>
2025-04-07 09:28:46 -0700

    Update main.cpp

--- CppBasicExample/src/main.cpp
+++ CppBasicExample/src/main.cpp
@@ -1,8 +1,27 @@
 #include "../include/main.h"
 
-int main(){
+struct Student{
+	string name;
+	int age;
+	int score;
+};
 
-	cout<<"hello world!"<< endl;
+
+int main(){
+	//Method #1 to create struct
+	struct Student s1;
+	s1.name = "Robin";
+	s1.age = 15;
+	s1.score = 99;
+	
+	//Method #2 to create struct
+	struct Student s2 = {"Connor", 18, 100};
+
+	
+	//cout<<"hello world!"<< endl;
+	cout<<s1.name<<", "<<s1.age<<", "<<s1.score<<endl;
+	cout<<s2.name<<", "<<s2.age<<", "<<s2.score<<endl;
+	
 	cout<<endl;
-    return 0;
+	return 0;
 }
