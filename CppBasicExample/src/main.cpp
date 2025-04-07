fe53042ed98bf59fadc1d513c8a54d331ce6fbe6
王哥流浪天涯 <wxj995@gmail.com>
2025-04-07 10:38:07 -0700

    Update main.cpp

--- CppBasicExample/src/main.cpp
+++ CppBasicExample/src/main.cpp
@@ -6,6 +6,13 @@ struct Student{
 	int score;
 }s3;
 
+struct teacher{ //a struct contains another struct
+	int id;
+	string name;
+	int age;
+	struct student stu;
+
+};
 
 int main(){
 	//Method #1 to create struct
@@ -32,13 +39,22 @@ int main(){
 	//Use pointer to visit struct
 	Student *p = &s1;
 
-	
-	cout<<"Print result on the screen!"<< endl;
+	cout<<"Print student result on the screen!"<< endl;
 	cout<<"s1 Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
 	cout<<"s2 Name:"<<s2.name<<", Age="<<s2.age<<", Score="<<s2.score<<endl;
 	cout<<"s3 Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl;
 	for(int i = 0; i < 3; i++) cout<<"studentArray["<<i<<"] Name:"<<studentArray[i].name<<", Age="<<studentArray[i].age<<", Score="<<studentArray[i].score<<endl;
 	cout<<"p Name:"<<p->name<<", Age="<<p->age<<", Score="<<p->score<<endl;
+
+	teacher t;
+	t.id = 10000;
+	t.name = "Garrett";
+	t.age = 50;
+	t.stu.name = "Hannah";
+	t.stu.age = 20;
+	t.stu.score = 60;
+	cout<<"t Name:"<<t.name<<", Age="<<t.age<<", Score="<<t.score<<endl;
+	cout<<"t.stu Name:"<<t.stu.name<<", Age="<<t.stu.age<<", Score="<<t.stu.score<<endl;
 
 	
 	cout<<endl;
