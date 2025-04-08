842c330299afd442ecd134fc764d95f3aa932483
王哥流浪天涯 <wxj995@gmail.com>
2025-04-08 15:59:52 -0700

    Update functionTest.h

--- CppBasicExample/include/functionTest.h
+++ CppBasicExample/include/functionTest.h
@@ -3,8 +3,16 @@
 
 #include "common.h"
 
-int func(int a, int b = 10, int c = 10){ //in C++, function can have default input vlaues
+int func1_default(int a, int b = 10, int c = 10){ //in C++, function can have default input vlaues
   return a + b + c;
+}
+
+int func2_overload(){ //in C++, function can overload
+  cout<<"Call func2_overload()"<<endl;
+}
+
+int func2_overload(int a){
+  cout<<"Call func2_overload(int a)"<<endl;
 }
 
 #endif
