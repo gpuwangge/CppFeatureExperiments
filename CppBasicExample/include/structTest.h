6318f8a96a706241e7eb158d4922b14e898cd63b
Connor <wxj995@gmail.com>
2025-04-08 01:35:34 -0700

    add structTest

---
+++ CppBasicExample/include/structTest.h
@@ -0,0 +1,43 @@
+#ifndef __STRUCTTEST_H__
+#define __STRUCTTEST_H__
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct Student{
+	string name;
+	int age;
+	int score;
+}s3;
+
+struct Teacher{ //a struct contains another struct
+	int id;
+	string name;
+	int age;
+	Student stu;
+
+};
+
+void PrintStudent_Value(Student s){ //pass struct by value
+	s.name = "Hacked";
+	cout<<"s(in function) Name:"<<s.name<<", Age="<<s.age<<", Score="<<s.score<<endl;
+}
+
+void PrintStudent_Address(Student *s){ //pass struct by address
+	s->name = "Hacked";
+	cout<<"s(in function) Name:"<<s->name<<", Age="<<s->age<<", Score="<<s->score<<endl;
+}
+
+void PrintStudent_Reference(Student &s){ //pass struct by reference
+	s.name = "Hacked";
+	cout<<"s(in function) Name:"<<s.name<<", Age="<<s.age<<", Score="<<s.score<<endl;
+}
+
+void PrintStudent_Address_Const(const Student *s){ //pass struct by address
+	//s->name = "Hacked"; //will trigger error
+	cout<<"s(in function) Name:"<<s->name<<", Age="<<s->age<<", Score="<<s->score<<endl;
+}
+
+
+#endif
--- CppBasicExample/src/main.cpp
+++ CppBasicExample/src/main.cpp
@@ -1,38 +1,6 @@
 #include "../include/main.h"
+#include "../include/structTest.h"
 
-struct Student{
-	string name;
-	int age;
-	int score;
-}s3;
-
-struct Teacher{ //a struct contains another struct
-	int id;
-	string name;
-	int age;
-	Student stu;
-
-};
-
-void PrintStudent(Student s){ //pass struct by value
-	s.name = "Hacked";
-	cout<<"s(in function) Name:"<<s.name<<", Age="<<s.age<<", Score="<<s.score<<endl;
-}
-
-void PrintStudent(Student *s){ //pass struct by address
-	s->name = "Hacked";
-	cout<<"s(in function) Name:"<<s->name<<", Age="<<s->age<<", Score="<<s->score<<endl;
-}
-
-void PrintStudent(Student &s){ //pass struct by reference
-	s.name = "Hacked";
-	cout<<"s(in function) Name:"<<s.name<<", Age="<<s.age<<", Score="<<s.score<<endl;
-}
-
-void PrintStudent(const Student *s){ //pass struct by address
-	//s->name = "Hacked"; //will trigger error
-	cout<<"s(in function) Name:"<<s->name<<", Age="<<s->age<<", Score="<<s->score<<endl;
-}
 
 int main(){
 	cout<<"Practise 1: 4 methods to create struct objects"<< endl;
@@ -54,25 +22,25 @@ int main(){
 	Student studentArray[3] {
 		{"Daniel", 35, 99},
 		{"Eddie", 22, 66},
-		{"Fiona, 32, 85"}
-	}
+		{"Fiona", 32, 85}
+	};
 
 	cout<<"Print student result on the screen!"<< endl;
 	cout<<"s1 Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
 	cout<<"s2 Name:"<<s2.name<<", Age="<<s2.age<<", Score="<<s2.score<<endl;
 	cout<<"s3 Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl;
 	for(int i = 0; i < 3; i++) cout<<"studentArray["<<i<<"] Name:"<<studentArray[i].name<<", Age="<<studentArray[i].age<<", Score="<<studentArray[i].score<<endl;
-
+	cout<<endl;
 
 	
 	cout<<"Practise 2: Use pointer to visit struct object"<< endl;
 	Student *p = &s1;
-	cout<<"p Name:"<<p->name<<", Age="<<p->age<<", Score="<<p->score<<endl;
+	cout<<"p Name:"<<p->name<<", Age="<<p->age<<", Score="<<p->score<<endl<<endl;
 
 
 	
 	cout<<"Practise 3: A struct(teacher) contains another struct(student)"<< endl;
-	teacher t;
+	Teacher t;
 	t.id = 10000;
 	t.name = "Garrett";
 	t.age = 50;
@@ -81,32 +49,32 @@ int main(){
 	t.stu.score = 60;
 
 	cout<<"Print teacher result on the screen!"<< endl;
-	cout<<"t Name:"<<t.name<<", Age="<<t.age<<", Score="<<t.score<<endl;
-	cout<<"t.stu Name:"<<t.stu.name<<", Age="<<t.stu.age<<", Score="<<t.stu.score<<endl;
+	cout<<"t Name:"<<t.name<<", Age="<<t.age<<", Id="<<t.id<<endl;
+	cout<<"t.stu Name:"<<t.stu.name<<", Age="<<t.stu.age<<", Score="<<t.stu.score<<endl<<endl;
 
 
 	
 	cout<<"Practise 4: Pass struct object as parameter"<< endl;
 	//pass struct by value
-	PrintStudent(s1);
+	PrintStudent_Value(s1);
 	cout<<"s1(in main) Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
 
 	//pass struct by address
-	PrintStudent(&s2);
+	PrintStudent_Address(&s2);
 	cout<<"s2(in main) Name:"<<s2.name<<", Age="<<s2.age<<", Score="<<s2.score<<endl;
 	
 	//pass struct by reference
-	PrintStudent(s3);
-	cout<<"s3(in main) Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl;
+	PrintStudent_Reference(s3);
+	cout<<"s3(in main) Name:"<<s3.name<<", Age="<<s3.age<<", Score="<<s3.score<<endl<<endl;
 
 
 	
 	cout<<"Practise 5: Use const in struct"<< endl;
-	PrintStudent(s1);
-	cout<<"s1(in main) Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl;
+	PrintStudent_Address_Const(&s1);
+	cout<<"s1(in main) Name:"<<s1.name<<", Age="<<s1.age<<", Score="<<s1.score<<endl<<endl;
 
 
 	
-	cout<<endl;
+	
 	return 0;
 }
