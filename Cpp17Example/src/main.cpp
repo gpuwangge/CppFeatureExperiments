c6eb11eb319591583d8b54abfc8ed8e049e49a1c
Connor <wxj995@gmail.com>
2025-04-17 17:09:48 -0700

    create empty framework for cpp11, 17, 20 and 23

---
+++ Cpp17Example/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/common.h"
+// #include "../include/structTest.h"
+// #include "../include/functionTest.h"
+// #include "../include/classTest.h"
+// #include "../include/pcBuilding.h"
+// #include "../include/customArray.h"
+
+int main(){
+	cout<<"hello world!"<<endl;
+	cout<<endl;
+	
+	return 0;
+}
---
+++ Cpp20Example/include/common.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <iostream>
+#include <string.h>
+#include <memory>
+using namespace std;
+
+#endif
